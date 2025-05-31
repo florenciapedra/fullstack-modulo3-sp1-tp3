112ca935ef6e10d8a0d04c59c34cfbaacbebe500
Florencia Pedraza Aguero <florenciapedrazaaguero@gmail.com>
2025-05-30 22:11:08 -0300

    Agregar ejercicios con Express

---
+++ express/server.mjs
@@ -0,0 +1,21 @@
+import express from 'express';
+
+// Crear una instancia de Express
+const app = express();
+
+// Configurar el puerto en el que el servidor escuchará
+const PORT = 3000;
+
+// Ruta básica
+app.get('/', (req, res) => {
+        res.send('¡Hola, mundo!');
+});
+
+app.get('/data', (req, res) =>{
+        res.send('Datos recibidos');
+});
+
+// Iniciar el servidor
+app.listen(PORT, () =>{
+        console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
